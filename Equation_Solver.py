4c1787eb20a07cf5a939c9d4a6305c329b67021d
Zachary Lem <zacharylem@users.noreply.github.com>
2020-11-11 12:52:55 -0500

    Update Equation_Solver.py

--- Equation_Solver.py
+++ Equation_Solver.py
@@ -5,41 +5,88 @@
 #   > Should there be a setting for significant digits or just stick to default?
 # - Image will be converted to either grayscale or monochrome
 #   > Is it possible for all input methods to achieve the data type for the image?
+# - IF WE'RE AIMING FOR A USER-FRIENDLY HMI, maybe add a 'Help' button?
+# CAUTION: You may need to install the following packages:
+# tkinter, numpy, requests, opencv, PILLOW
 
-#test push
+# Libraries
+import cv2
+import os
+import numpy
+import PIL.Image, PIL.ImageTk
 
-# Necessary Libraries for HMI
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 
+# Function Definitions
 # (WIP) Clean the HMI if user decides to switch input type
 def reset_HMI():
+    canvas.delete("all")
+
     if input_type.get() != "photo":
         browse_btn.pack_forget()
-        file_label.pack_forget()        
+        file_label.pack_forget()  
+        photo_txt.pack_forget()      
     
     if input_type.get() != "phone":
         phone_label.pack_forget()
+        phone_txt.pack_forget()
         
     if input_type.get() != "canvas":
         clear_btn.pack_forget()
-        
-    if input_type.get() != "photo" or input_type.get() != "phone":
-        in_txt.pack_forget()
 
 # Open file explorer and get photo of equation  
 def locate_photo():
+    # Open file explorer
     Tk().withdraw()
     photoname = askopenfilename()
-    in_txt.delete(0, END)
-    in_txt.insert(0, photoname)
+
+    # Write image location in textbox
+    photo_txt.delete(0, END)
+    photo_txt.insert(0, photoname)
+
+    # Rerun the method to execute AI and image algorithms
+    upload_photo()
 
 # (WIP) Getting a photo off your computer
 def upload_photo():
     reset_HMI()
     file_label.pack(side=LEFT)
-    in_txt.pack(side=LEFT)
+    photo_txt.pack(side=LEFT)
     browse_btn.pack(side=LEFT)
+    error_label.config(text="")
+
+    # Grab image location from textbox
+    try: img = cv2.cvtColor(cv2.imread(os.path.join(photo_txt.get())), cv2.COLOR_BGR2RGB)
+    except: img = cv2.imread(os.path.join(photo_txt.get()))
+
+    if img is not None: # If a image file has been found at location...
+        # Defining image scaling factors/parameters
+        scale_width = canvas_width / img.shape[1]
+        scale_height = canvas_height / img.shape[0]
+        xOffset = 0
+        yOffset = 0
+
+        # Determining new size and position of rescaled image
+        if scale_width <= scale_height:
+            scale_height = int(img.shape[0] * scale_width)
+            scale_width = int(img.shape[1] * scale_width)            
+            yOffset = int(canvas_height/2 - scale_height/2)
+        else:
+            scale_width = int(img.shape[1] * scale_height)
+            scale_height = int(img.shape[0] * scale_height)
+            xOffset = int(canvas_width/2 - scale_width/2)
+        dim = (scale_width, scale_height)
+        resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+                
+        # Display the preview on canvas
+        if resized is not None:
+            preview = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(resized))
+            canvas.create_image(500, 125, image=preview)
+            mainloop()
+            # Run AI method here
+    else: #...otherwise display error
+        error_label.config(text="Error: Image can not be found!")
     
 # (WIP) Getting access to your web camera
 def display_webcam():
@@ -50,7 +97,7 @@ def display_webcam():
 def display_phone():
     reset_HMI()
     phone_label.pack(side=LEFT)
-    in_txt.pack(side=LEFT)
+    phone_txt.pack(side=LEFT)
     
 # (WIP) Allowing user to write onto a canvas provided by HMI
 def write_on_canvas():
@@ -87,11 +134,18 @@ input_frame.pack(side=TOP)
 # Specific control widgets for the upload photo option
 browse_btn = Button(input_frame, text="Browse", command=locate_photo)
 file_label = Label(input_frame, text="File Location: ")
-in_txt = Entry(input_frame, width=50) # Also used for the phone camera option
+photo_txt = Entry(input_frame, width=50)
 
 phone_label = Label(input_frame, text="[ANDROID ONLY] IPv4 Address (From IP Webcam App): ")
+phone_txt = Entry(input_frame, width=50)
 
 clear_btn = Button(input_frame, text="Clear") #, command=canvas.delete("all")) #?
+
+# Frame for error dialog
+error_frame = Frame(hmi)
+error_frame.pack(side=BOTTOM)
+error_label = Label(error_frame, text="")
+error_label.pack()
 
 # Defining canvas boundaries and putting it into the HMI
 canvas_width = 1000
